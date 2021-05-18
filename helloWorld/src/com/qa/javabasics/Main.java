bd58d53f5ef27e1dc51ce88b073ce1651b9e98a9
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2021-05-18 13:51:36 -0500

    wordCount complete

--- helloWorld/src/com/qa/javabasics/Main.java
+++ helloWorld/src/com/qa/javabasics/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		WordCount.wordCount("booger");
-
+			Results.getPercentage();
+			
 	}
 
 }
--- helloWorld/src/com/qa/javabasics/StringManipulation.java
+++ helloWorld/src/com/qa/javabasics/StringManipulation.java
@@ -1,15 +1,31 @@
 package com.qa.javabasics;
 
-public class WordCount {
+public class StringManipulation {
 
-	public static void wordCount(String word) {
+	String str1 = "yesterday it was raining";
+	String str2 = "today it is sunny";
 
-		System.out.println(word.length());
+	String mySubString1 = str2.substring(0, 11);
+	String mySubString2 = str1.substring(16, 24);
+
+	
+	// Method 1 - When given a String, count and return how many words there are in
+	// that String.
+	public static int wordCount(String s) {
+
+		if (s.length() == 0)
+			return 0;
+		int words = 1;
+		for (int i = 0; i < s.length(); i++) {
+			String current = s.substring(i, i + 1);
+			if (current.equals(" ")) {
+				words++;
+			}
+		}
+		return words;
 
 	}
 
-	// Method 1 - When given a String, count and return how many words there are in
-	// that String.
 	// Method 2 - When given a String, print out this String in a vertical fashion,
 	// each word on a different line.
 	// Method 3 - When given a String, print out this String in a vertical fashion
